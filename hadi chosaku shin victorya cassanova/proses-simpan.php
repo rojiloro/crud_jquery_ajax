c4b952afc5efe3af3b8f6c210b3bb27efb98a415
hadipsy27 <33802468+hadipsy27@users.noreply.github.com>
2018-05-01 23:09:53 -0700

    Update proses-simpan.php

--- hadi chosaku shin victorya cassanova/proses-simpan.php
+++ hadi chosaku shin victorya cassanova/proses-simpan.php
@@ -1,14 +1,3 @@
- <!-- Aplikasi CRUD
- ************************************************
- * Developer    : Indra Styawantoro
- * Company      : Indra Studio
- * Release Date : 1 Maret 2016
- * Website      : http://www.indrasatya.com, http://www.kulikoding.net
- * E-mail       : indra.setyawantoro@gmail.com
- * Phone        : +62-856-6991-9769
- * BBM          : 7679B9D9
- -->
-
 <?php
 // Panggil koneksi database
 require_once "config/database.php";
